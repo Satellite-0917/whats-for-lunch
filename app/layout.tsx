6795d41202ac4aff6e50462397ca63b6256ba000
Satellite-0917 <gimsuhyeon694@gmail.com>
2026-01-31 03:08:34 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { ReactNode } from 'react';
+import Script from 'next/script';
 
 export const metadata = {
   title: '오늘 뭐 먹지?',
@@ -9,6 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko" suppressHydrationWarning>
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
